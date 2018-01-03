32b15cea7040f71168ac18253b799624f7d588ec
Seatis <atikezer@gmail.com>
2018-01-03 12:00:50 +0100

    feat(frontend.js): callback (appendTable) function is created

--- frontend.js
+++ frontend.js
@@ -1,3 +1,27 @@
+function appendTable(result){
+  var table = document.querySelector('table');
+  table.innerHTML = '';
+  table.innerHTML = `<tr>
+                      <th>Plate</th>
+                      <th>Brand</th> 
+                      <th>Model</th>
+                      <th>Color</th>
+                      <th>Year</th>
+                    </tr>
+`
+  result.forEach(function(element) {
+  const markup = `<tr>
+                    <td>${element.plate}</td>
+                    <td>${element.car_brand}</td>
+                    <td>${element.car_model}</td>
+                    <td>${element.color}</td>
+                    <td>${element.year}</td>
+                  </tr>
+  `
+  table.innerHTML += markup;
+  }, this);
+}
+
 function ajax (method, url, data, callback) {
   var xhr = new XMLHttpRequest();
   xhr.addEventListener("readystatechange", function () {
