609501ce166c7e2d93d1aa57de9c739f1736ec3d
Seatis <atikezer@gmail.com>
2018-01-03 11:58:53 +0100

    feat(frontend.js): ajax function is created

---
+++ frontend.js
@@ -0,0 +1,11 @@
+function ajax (method, url, data, callback) {
+  var xhr = new XMLHttpRequest();
+  xhr.addEventListener("readystatechange", function () {
+    if (this.readyState === 4) {
+      var myObj = JSON.parse(this.responseText);
+      callback(myObj);
+    }
+  });
+  xhr.open(method, url);
+  xhr.send(data);
+}
