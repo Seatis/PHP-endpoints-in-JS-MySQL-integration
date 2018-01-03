fd3031460be3f8f142360a0c47f5a3b5fd86f5f6
Seatis <atikezer@gmail.com>
2018-01-03 11:55:40 +0100

    feat(backend.php): endpoints and querys are defined, data is fetched

--- backend.php
+++ backend.php
@@ -5,4 +5,17 @@ foreach($_GET as $key => $value) {
 	$$key=$value;
 }
 
+if (isset($color)) {
+  $sql="SELECT * FROM licence_plates WHERE color=$color";
+} elseif (isset($brand)) {
+  $sql="SELECT * FROM licence_plates WHERE car_brand=$brand";
+} else {
+  $sql="SELECT * FROM licence_plates";
+}
+
+$result = mysqli_query($conn,$sql);
+
+while($row = mysqli_fetch_array($result,MYSQLI_ASSOC)) {
+  $rows[] = $row;
+}
 ?>
